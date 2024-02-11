ecb2dddc1e64f064705b09bc9c4d2cdef29c3c89
Craig Miller <craigmiller160@gmail.com>
2024-02-11 14:43:18 -0500

    Working on python script

---
+++ get_certs/get_certs.py
@@ -0,0 +1,30 @@
+import yaml
+import subprocess
+
+
+def get_certificate_secret():
+    process = subprocess.run([
+        'kubectl', '-n', 'infra-prod', 'get', 'secret', 'kafka-certificate', '-o', 'yaml'
+    ], capture_output=True, text=True)
+
+    return process.stdout
+
+
+def extract_certs(yaml_string):
+    yaml_object = yaml.safe_load(yaml_string)
+    return yaml_object['data']
+
+
+def write_to_file(file_name, content):
+    with open(file_name, 'wb') as file:
+        file.write(content)
+
+
+def write_certs(certs):
+    for file_name, content in certs.items():
+        write_to_file(file_name, content)
+
+
+secret_yaml_string = get_certificate_secret()
+secret_certs = extract_certs(secret_yaml_string)
+write_certs(secret_certs)
