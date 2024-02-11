2a5d1e5421f557af0799fdb999d6898dbe36bb89
Craig Miller <craigmiller160@gmail.com>
2024-02-11 14:45:37 -0500

    Got the certs fully extracted

--- get_certs/get_certs.py
+++ get_certs/get_certs.py
@@ -19,7 +19,7 @@ def extract_certs(yaml_string):
 def write_to_file(file_name, content):
     decoded_content = base64.b64decode(content)
     with open(file_name, 'wb') as file:
-        file.write(content)
+        file.write(decoded_content)
 
 
 def write_certs(certs):
